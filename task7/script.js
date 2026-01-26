5d1365c78c1ccdb4b255113ece90cfff8fe9046d
Щегольков С.Д. <shchegolkov_sd@lgnch.spb.ru>
2026-01-26 15:11:56 +0300

    test pre-commit hook1

--- task7/script.js
+++ task7/script.js
@@ -36,5 +36,5 @@ function initApp() {
 // Экспортируем для тестов ...
 if (typeof module !== 'undefined' && module.exports) {
   module.exports = { checkInput, initApp };
-  //add some comment
+  //some comment
 }
